8040747659311e4c4b236708d70bd9c100cd68e6
Anonymous <>
2022-02-12 16:28:58 +0100

    Implement display one Square

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Square from "../Square";
 
 const Board = () => {
-
   return (
     <div data-testid="board-container" id="board-container">
+      <Square />
     </div>
   );
 };
