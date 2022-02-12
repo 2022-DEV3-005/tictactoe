9f19da7ff889da60d2ec7657f0545130700e6454
Anonymous <>
2022-02-12 20:59:47 +0100

    Test1: Player X as winner Test2: Player O as winner

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,4 +1,4 @@
-import { screen, fireEvent } from "@testing-library/react";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
 
 import { render } from "../../utils/test-utils";
 import Game from ".";
@@ -21,21 +21,50 @@ describe("Game", () => {
     fireEvent.click(squares[0]);
     expect(squares[0].textContent).toBe("X");
   });
-});
+  test("Switch players", () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    fireEvent.click(squares[0]);
+    fireEvent.click(squares[1]);
+    expect(squares[0].textContent).toBe("X");
+    expect(squares[1].textContent).toBe("O");
+  });
 
-test("Switch players", () => {
-  render(<Game />);
-  const squares = screen.queryAllByTestId("square");
-  fireEvent.click(squares[0]);
-  fireEvent.click(squares[1]);
-  expect(squares[0].textContent).toBe("X");
-  expect(squares[1].textContent).toBe("O");
-});
+  test("should be immutable if played, cannot play on a played position", () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    fireEvent.click(squares[0]);
+    fireEvent.click(squares[0]);
+    expect(squares[0].textContent).toBe("X");
+  });
 
-test("should be immutable if played, cannot play on a played position", () => {
-  render(<Game />);
-  const squares = screen.queryAllByTestId("square");
-  fireEvent.click(squares[0]);
-  fireEvent.click(squares[0]);
-  expect(squares[0].textContent).toBe("X");
+  test("X as winner", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [8, 7, 5, 4, 2].forEach((x) => fireEvent.click(squares[x]));
+    await waitFor(
+      () => {
+        expect(screen.getByTestId("status").textContent).toBe(
+          "Game Over: The winner is X"
+        );
+      },
+      1000,
+      50
+    );
+  });
+
+  test("O as winner", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [8, 7, 5, 4, 6, 1].forEach((x) => fireEvent.click(squares[x]));
+    await waitFor(
+      () => {
+        expect(screen.getByTestId("status").textContent).toBe(
+          "Game Over: The winner is O"
+        );
+      },
+      1000,
+      50
+    );
+  });
 });
