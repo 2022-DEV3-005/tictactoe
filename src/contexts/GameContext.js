1bad52a13fde931d4f178a04f81a204c154806de
Anonymous <>
2022-02-12 20:17:19 +0100

    Implement switching Players and Players can't play on a played position

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -10,18 +10,31 @@ export function useGameContext() {
   return useContext(GameContext);
 }
 
+//Two Players X and O
+export const PLAYER = {
+  X: "X",
+  O: "O",
+};
+
 //GameContext Provider
 export function GameContextProvider({ children }) {
   const [grid, setGrid] = useState();
+  const [selectedPlayer, setSelectedPlayer] = useState(PLAYER.X);
 
-  // Initializing the Game once after the mounting 
+  React.useEffect(() => {
+    grid && switchPlayer();
+  }, [grid]);
+
+  // Initializing the Game once after the mounting
   useEffect(() => {
     initialiseGame();
   }, []);
 
-  /** 
-   *   - gridMap : is Map object that contains an array of 9 elements where each element is stored in the same order as inserted as 
-   *               a Key / value => *   index / {position}  [0] [1] [2] [3] [4] [5] [6] [7] [8] 
+  /**
+   *   - gridMap : is Map object that contains an array of 9 elements where each element is stored in the same order as inserted as
+   *               a Key / value => *   index / {position} after we will [0] [1] [2] [3] [4] [5] [6] [7] [8]
+   *
+   *   - selectedPlayer : X always goes first at the start of the game
    */
   const initialiseGame = () => {
     const gridMap = new Map(grid);
@@ -29,11 +42,20 @@ export function GameContextProvider({ children }) {
       gridMap.set(index, { position: index });
     });
     setGrid(gridMap);
+    setSelectedPlayer(PLAYER.X);
+  };
+
+  const switchPlayer = () => {
+    const canSwitchPlayer = !![...grid.values()].find((square) => square.owner);
+    if (canSwitchPlayer) {
+      setSelectedPlayer(selectedPlayer === PLAYER.X ? PLAYER.O : PLAYER.X);
+    }
   };
 
   const value = {
     grid,
     setGrid,
+    selectedPlayer,
   };
 
   return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
