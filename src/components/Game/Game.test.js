41e16b42caee5f875cdf4ee8a2cd0aeeecbd4fa6
Anonymous <>
2022-02-12 15:42:04 +0100

    Test: Rendering Game view with title

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,7 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import Game from '.';
+import { render, screen } from "@testing-library/react";
+import Game from ".";
 
-test("should render Game view", () => {
-  render(<Game />);
-  expect(screen.getByTestId(/^game-container/)).toBeInTheDocument();
+describe("Game", () => {
+  test("Should render Game view with Tic Tac Toe title", () => {
+    render(<Game />);
+    expect(screen.getByTestId(/^game-container/)).toBeInTheDocument();
+    expect(screen.getByTestId(/^game-title/)).toBeInTheDocument();
+    const title = screen.getByText(/Tic Tac Toe/i);
+    expect(title).toBeInTheDocument();
+  });
 });
