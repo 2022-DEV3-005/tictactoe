d57ffacac374513aec40bea7a41ee9b05698857d
Anonymous <>
2022-02-12 18:31:06 +0100

    Test: Display a grid of 9 squares

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { screen } from "@testing-library/react";
-import Board from ".";
 import { render } from "../../utils/test-utils";
+import Board from ".";
 
 describe("Board", () => {
   test("should render a Board", () => {
     render(<Board />);
     expect(screen.getByTestId(/^board-container/)).toBeInTheDocument();
+  });
+
+  test("should display a grid of 9 squares", () => {
+    render(<Board />);
+    const squares = screen.queryAllByTestId("square");
+    expect(squares.length).toBe(9);
   });
 });
