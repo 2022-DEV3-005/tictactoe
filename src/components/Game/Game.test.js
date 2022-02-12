78b25c46b3ddea87a5deabc0c559a031037fdeae
Anonymous <>
2022-02-12 22:28:36 +0100

    Test1: Player has three in a row horizontally, vertically or diagonally

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -67,4 +67,124 @@ describe("Game", () => {
       50
     );
   });
+
+  /**  [0*, 1*, 2*]
+   *   [3 , 4 , 5 ]
+   *   [6 , 7,  8 ]
+   */
+  test("player O win after mark 3 squares in first row", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [3, 0, 4, 1, 6, 2].forEach((x) => fireEvent.click(squares[x]));
+    expect(screen.getByTestId("status").textContent).toBe(
+      "Game Over: The winner is O 💪"
+    );
+    const retryBtn = screen.getByTestId(/^retry-btn/);
+    expect(retryBtn).toBeInTheDocument();
+  });
+
+  /**  [0 , 1 , 2 ]
+   *   [3*, 4*, 5*]
+   *   [6 , 7 , 8 ]
+   */
+  test("player X win after mark 3 squares in second row", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [3, 0, 4, 1, 5].forEach((x) => fireEvent.click(squares[x]));
+    expect(screen.getByTestId("status").textContent).toBe(
+      "Game Over: The winner is X 💪"
+    );
+    const retryBtn = screen.getByTestId(/^retry-btn/);
+    expect(retryBtn).toBeInTheDocument();
+  });
+
+  /**  [0 , 1 , 2 ]
+   *   [3 , 4 , 5 ]
+   *   [6*, 7*, 8*]
+   */
+  test("player O win after mark 3 squares in third row", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [3, 6, 4, 7, 0, 8].forEach((x) => fireEvent.click(squares[x]));
+    expect(screen.getByTestId("status").textContent).toBe(
+      "Game Over: The winner is O 💪"
+    );
+    const retryBtn = screen.getByTestId(/^retry-btn/);
+    expect(retryBtn).toBeInTheDocument();
+  });
+
+  /**  [0*, 1, 2]
+   *   [3*, 4, 5]
+   *   [6*, 7, 8]
+   */
+  test("player X win after mark 3 squares in first column", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [0, 1, 3, 4, 6].forEach((x) => fireEvent.click(squares[x]));
+    expect(screen.getByTestId("status").textContent).toBe(
+      "Game Over: The winner is X 💪"
+    );
+    const retryBtn = screen.getByTestId(/^retry-btn/);
+    expect(retryBtn).toBeInTheDocument();
+  });
+
+  /**  [0, 1*, 2]
+   *   [3, 4*, 5]
+   *   [6, 7*, 8]
+   */
+  test("player O win after mark 3 squares in second column", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [0, 1, 3, 4, 8, 7].forEach((x) => fireEvent.click(squares[x]));
+    expect(screen.getByTestId("status").textContent).toBe(
+      "Game Over: The winner is O 💪"
+    );
+    const retryBtn = screen.getByTestId(/^retry-btn/);
+    expect(retryBtn).toBeInTheDocument();
+  });
+
+  /**  [0, 1, 2*]
+   *   [3, 4, 5*]
+   *   [6, 7, 8*]
+   */
+  test("player X win after mark 3 squares in third column", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [2, 1, 5, 4, 8].forEach((x) => fireEvent.click(squares[x]));
+    expect(screen.getByTestId("status").textContent).toBe(
+      "Game Over: The winner is X 💪"
+    );
+    const retryBtn = screen.getByTestId(/^retry-btn/);
+    expect(retryBtn).toBeInTheDocument();
+  });
+
+  /**  [0*, 1, 2]
+   *   [3, 4*, 5]
+   *   [6, 7, 8*]
+   */
+  test("player X win after mark 3 squares in first diagonal", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [0, 1, 4, 2, 8].forEach((x) => fireEvent.click(squares[x]));
+    expect(screen.getByTestId("status").textContent).toBe(
+      "Game Over: The winner is X 💪"
+    );
+    const retryBtn = screen.getByTestId(/^retry-btn/);
+    expect(retryBtn).toBeInTheDocument();
+  });
+
+  /**  [0, 1, 2*]
+   *   [3, 4*, 5]
+   *   [6*, 7, 8]
+   */
+  test("player O win after mark 3 squares in second diagonal", async () => {
+    render(<Game />);
+    const squares = screen.queryAllByTestId("square");
+    [0, 2, 1, 4, 3, 6].forEach((x) => fireEvent.click(squares[x]));
+    expect(screen.getByTestId("status").textContent).toBe(
+      "Game Over: The winner is O 💪"
+    );
+    const retryBtn = screen.getByTestId(/^retry-btn/);
+    expect(retryBtn).toBeInTheDocument();
+  });
 });
