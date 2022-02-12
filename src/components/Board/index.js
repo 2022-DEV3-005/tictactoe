194b77ae9fcaeb249f646c1869441fbfd18deb32
Anonymous <>
2022-02-12 18:32:15 +0100

    Implement display a grid of 9 squares

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useGameContext } from "../../contexts/GameContext";
 import Square from "../Square";
 
 const Board = () => {
+  const { grid} = useGameContext();
+
+  const renderGrid = () => {
+    return [...grid.entries()].map(([squareIndex, square]) => (
+      <Square key={squareIndex} square={square} />
+    ));
+  };
   return (
     <div data-testid="board-container" id="board-container">
-      <Square />
+      {grid && renderGrid()}
     </div>
   );
 };
--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -1,15 +1,40 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { node } from "prop-types";
 
+//GameContext
 const GameContext = createContext();
 
+//GameContext Consumer
 export function useGameContext() {
   return useContext(GameContext);
 }
 
+//GameContext Provider
 export function GameContextProvider({ children }) {
-  const value = {};
+  const [grid, setGrid] = useState();
+
+  // Initializing the Game once after the mounting 
+  useEffect(() => {
+    initialiseGame();
+  }, []);
+
+  /** 
+   *   - gridMap : is Map object that contains an array of 9 elements where each element is stored in the same order as inserted as 
+   *               a Key / value => *   index / {position}  [0] [1] [2] [3] [4] [5] [6] [7] [8] 
+   */
+  const initialiseGame = () => {
+    const gridMap = new Map(grid);
+    [...Array(9)].forEach((_, index) => {
+      gridMap.set(index, { position: index });
+    });
+    setGrid(gridMap);
+  };
+
+  const value = {
+    grid,
+    setGrid,
+  };
 
   return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
 }
